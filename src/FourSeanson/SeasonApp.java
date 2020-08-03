bfe9b73139aa0f2adaae2d1a3e62aa4192b750c6
Ewa Gauze <gauze.ewa@gmail.com>
2020-08-03 09:49:14 +0200

    add all classes

---
+++ src/FourSeanson/SeasonApp.java
@@ -0,0 +1,8 @@
+package FourSeanson;
+
+public class SeasonApp {
+    public static void main(String[] args) {
+        MonthToSeason monthToSeason=new MonthToSeason();
+        monthToSeason.choseSeason();
+    }
+}
